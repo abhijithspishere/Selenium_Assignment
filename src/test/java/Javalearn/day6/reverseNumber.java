428d7fc58df0e96719b30fc4e775f61f29d7b45a
abhijithspishere <abhijithspishere@gmail.com>
2024-10-15 12:10:28 +0530

    String Builder

--- src/test/java/Javalearn/day6/reverseNumber.java
+++ src/test/java/Javalearn/day6/reverseNumber.java
@@ -3,7 +3,7 @@ package Javalearn.day6;
 public class reverseNumber {
     public static void main(String[] args) {
         //Using While Loop
-        int num =12241;
+        /*int num =12241;
         int rev = 0;
         while (num>0)
         {
@@ -11,6 +11,22 @@ public class reverseNumber {
             rev = rev*10+digit;
             num = num/10;
         }
-        System.out.println("The reversed number is "+rev);
+        System.out.println("The reversed number is "+rev);*/
+
+        //String Builder Insert
+        /*StringBuilder sb = new StringBuilder("VintagePhoto");
+        sb.insert(8,"Green ");
+        sb.delete(8,13);
+        sb.deleteCharAt(7);
+        sb.replace(0,7,"New");
+        sb.setLength(7);
+        System.out.println(sb); */
+
+        //Reversing a number using StringBuilder Form
+        int num = 1224;
+        String number = String.valueOf(num);
+        StringBuilder sb = new StringBuilder(number);
+        System.out.println(sb.reverse());
+
     }
 }
